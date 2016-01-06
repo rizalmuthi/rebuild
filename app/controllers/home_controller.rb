1720b2a913b460f2415597d8743368d742d49b5d
dferdian <me@dhendyferdian.com>
2016-01-06 23:39:17 +0800

    clean demo code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @message = "My Home"
-    render :index
+
   end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -1,5 +1,4 @@
 require "test_helper"
-require "active_record"
 require "user"
 
 class UserTest < ActiveSupport::TestCase
